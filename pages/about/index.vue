9cfc75dc8cedfc1fe1fd16931c1cd6291b859799
Kingsley Solomon <kinxly@gmail.com>
2022-09-29 04:36:47 +0100

    [about page 100%]

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,11 +1,60 @@
 <template>
-  <main>
+  <main class="about">
     <Banner
       title="About"
       img="https://res.cloudinary.com/kingsleysolomon/image/upload/v1664417563/webtech/Rectangle_4_whmbrs.png"
       :nav="nav"
     />
     <!-- SECTION TWO -->
+    <v-layout class="cc-wrapper v-spacing">
+      <v-card
+        flat
+        class="about_img_sec_2 rounded-0 mr-16"
+        width="470"
+        height="630"
+      >
+        <v-img
+          class="img-fluid"
+          src="https://res.cloudinary.com/kingsleysolomon/image/upload/v1664420899/webtech/Rectangle_5_u8qgms.png"
+          alt="img"
+        ></v-img>
+      </v-card>
+      <section class="pl-10">
+        <div class="py-5 mb-16">
+          <h2
+            style="font-size: 40px"
+            class="font-m accent--text left_border pl-7 py-2 font-weight-bold d-inline letter-spacing"
+          >
+            About
+          </h2>
+          <h2
+            style="font-size: 40px"
+            class="font-m py-2 font-weight-bold d-inline letter-spacing"
+          >
+            Us
+          </h2>
+        </div>
+        <p class="info--text" style="line-height: 30px">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed
+          laudantium ullam similique nobis adipisci magnam dicta rerum, et
+          numquam voluptatibus.
+        </p>
+        <section class="pl-10 my-10">
+          <v-row no-gutters>
+            <v-col cols="6" v-for="n in 4" :key="n">
+              <div class="pa-3 info--text">
+                <h4 class="mb-3 font-weight-bold" style="font-size: 20px">
+                  Awesome Features
+                </h4>
+                <p class="info--text" style="line-height: 24px">
+                  Easy-to-use, dynamic colors, grid, video, map and much more .
+                </p>
+              </div>
+            </v-col>
+          </v-row>
+        </section>
+      </section>
+    </v-layout>
     <!-- SECTION THREE -->
     <v-layout class="about_section_3">
       <v-layout flex-column class="cc-wrapper v-spacing">
