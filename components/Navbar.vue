ef5d707ccfe41f46a0434ba2804db9512717004d
Kingsley Solomon <kinxly@gmail.com>
2022-10-03 08:43:27 +0100

    [fixed button nav]

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,10 @@
 <template>
   <client-only>
     <v-sheet class="py-2" color="navbarBg">
-      <v-app-bar elevation="0" class="transparent">
+      <v-app-bar :fixed="setFixed" flat class="navbarBg">
         <v-layout class="cc-wrapper align-center pa-0">
           <v-toolbar-title class="pa-0">
-            <v-img
-              max-width="16.9rem"
-              src="/Frame.svg"
-              alt="logo"
-            ></v-img
+            <v-img max-width="16.9rem" src="/Frame.svg" alt="logo"></v-img
           ></v-toolbar-title>
           <v-spacer></v-spacer>
           <v-layout class="justify-space-between d-none d-md-flex">
@@ -25,27 +21,152 @@
           </v-icon>
         </v-layout>
       </v-app-bar>
+
       <v-navigation-drawer height="100%" temporary app v-model="state.drawer">
-        <v-sheet
-          height="100%"
-          class="d-flex flex-column align-center justify-space-around"
-        >
-          <NuxtLink to="/"> Home </NuxtLink>
-          <NuxtLink to="/about"> About </NuxtLink>
-          <NuxtLink to="/services"> Services </NuxtLink>
-          <NuxtLink to="/portfolio"> Portfolio </NuxtLink>
-          <NuxtLink to="/blog"> Blog </NuxtLink>
-          <NuxtLink to="/pricing"> Pricing </NuxtLink>
-          <NuxtLink to="/contact"> Contact Us </NuxtLink>
-        </v-sheet>
+        <v-list-item>
+          <v-list-item-content>
+            <v-img max-width="16.9rem" src="/Frame.svg" alt="logo"></v-img>
+            <v-list-item-subtitle
+              style="font-size: 6px !important; letter-spacing: 2px !important"
+              class="font-weight-bold text-overline info--text"
+            >
+              By Kingsley and Tobi</v-list-item-subtitle
+            >
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+          <NuxtLink to="/">
+            <v-list-item class="my-3">
+              <v-list-item-icon
+                style="height: fit-content !important"
+                class="accent pa-3 rounded-xl"
+              >
+                <v-icon dark small>{{ state.icon.home }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold"
+                  >Home</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </NuxtLink>
+
+          <NuxtLink to="/about">
+            <v-list-item class="my-3">
+              <v-list-item-icon
+                style="height: fit-content !important"
+                class="accent pa-3 rounded-xl"
+              >
+                <v-icon dark small>{{ state.icon.about }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold"
+                  >About</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </NuxtLink>
+          <NuxtLink to="/services">
+            <v-list-item class="my-3">
+              <v-list-item-icon
+                style="height: fit-content !important"
+                class="accent pa-3 rounded-xl"
+              >
+                <v-icon dark small>{{ state.icon.service }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold"
+                  >Service</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </NuxtLink>
+          <NuxtLink to="/portfolio">
+            <v-list-item class="my-3">
+              <v-list-item-icon
+                style="height: fit-content !important"
+                class="accent pa-3 rounded-xl"
+              >
+                <v-icon dark small>{{ state.icon.portfolio }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold"
+                  >Portfolio</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </NuxtLink>
+          <NuxtLink to="/blog">
+            <v-list-item class="my-3">
+              <v-list-item-icon
+                style="height: fit-content !important"
+                class="accent pa-3 rounded-xl"
+              >
+                <v-icon dark small>{{ state.icon.blog }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold"
+                  >Blog</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </NuxtLink>
+          <NuxtLink to="/pricing">
+            <v-list-item class="my-3">
+              <v-list-item-icon
+                style="height: fit-content !important"
+                class="accent pa-3 rounded-xl"
+              >
+                <v-icon dark small>{{ state.icon.pricing }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold"
+                  >Pricing</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </NuxtLink>
+          <NuxtLink to="/contact">
+            <v-list-item class="my-3">
+              <v-list-item-icon
+                style="height: fit-content !important"
+                class="accent pa-3 rounded-xl"
+              >
+                <v-icon dark small>{{ state.icon.contact }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold"
+                  >Contact Us</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </NuxtLink>
+        </v-list>
+        <v-divider></v-divider>
+        <section style="position: absolute; bottom: 0">
+          <p class="text-caption">Lorem ipsum dolor sit amet.</p>
+        </section>
       </v-navigation-drawer>
     </v-sheet>
   </client-only>
 </template>
 
 <script setup lang="ts">
-import { mdiMenu } from "@mdi/js";
+import {
+  mdiAccountCreditCard,
+  mdiFaceAgent,
+  mdiFileTree,
+  mdiHome,
+  mdiInformationVariant,
+  mdiMenu,
+  mdiPost,
+} from "@mdi/js";
 import { reactive } from "vue";
+
+defineProps({
+  setFixed: Boolean,
+});
 
 const state = reactive({
   firstName: `kingsley`,
@@ -53,6 +174,13 @@ const state = reactive({
   img: `https://images.unsplash.com/photo-1661961111247-e218f67d1cd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60`,
   icon: {
     menu: mdiMenu,
+    home: mdiHome,
+    about: mdiInformationVariant,
+    service: mdiFaceAgent,
+    portfolio: mdiFileTree,
+    blog: mdiPost,
+    pricing: mdiAccountCreditCard,
+    contact: mdiFaceAgent,
   },
   drawer: null,
 });
